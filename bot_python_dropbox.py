3084e8221604d2f43f383b6d5ef4ce46d178803c
Sebastian Chaves <jschavesh@gmail.comm>
2022-05-24 11:07:58 -0300

    added front-end based on kivyMD

--- bot_python_dropbox.py
+++ bot_python_dropbox.py
@@ -3,7 +3,7 @@ from macro_file import executar_macro, open_personal, tear_down_personal
 import os
 
 # Caminho para executar o Bot
-pasta = r"C:\Users\Mitsidi\Dropbox (Mitsidi Projetos)\05_TEC-MB_Dir+Ger+Anl\TEC_Plan.-Técnico\TEC_Gerenciamento-Projetos"
+pasta = r"C:\Users\Sebastian\Dropbox (Mitsidi Projetos)\05_TEC-MB_Dir+Ger+Anl\TEC_Plan.-Técnico\TEC_Gerenciamento-Projetos"
 
 # Codigo para gerar lista de todas os projeto terminados em xlsb
 caminhos = [os.path.join(pasta, nome) for nome in os.listdir(pasta)]
@@ -14,7 +14,7 @@ xlsb = [arq for arq in arquivos if arq.lower().endswith(".xlsb")]
 Atualizar_link = "Atualizar_link"
 
 # Abrir planilha com macro
-pensonal_path = r'\arquivos_excell\PRJ_2212_MVP_Piloto_AHK.xlsb'
+pensonal_path = os.path.dirname(__file__) + r'\arquivos_excell\PRJ_2212_MVP_Piloto_AHK.xlsb'
 wb_personal = open_personal(pensonal_path)
 
 # Looping para executar macro
--- macro_file.py
+++ macro_file.py
@@ -2,22 +2,24 @@ import xlwings as xw
 
 
 def executar_macro(excel_path, name_macro):
-    wb = xw.Book(excel_path, update_links = False)
+    wb = xw.Book(excel_path, update_links=False)
     app = wb.app
 
-    macro_vba = app.macro(f"'PERSONAL.XLSB'!{name_macro}") 
+    macro_vba = app.macro(f"'00_script_não_apagar.XLSB'!{name_macro}")
     macro_vba()
-    
+
     wb.save()
     wb.close()
 
 
 def open_personal(excel_path):
-    wb_personal = xw.Book(excel_path, update_links = False)
+    wb_personal = xw.Book(excel_path, update_links=False)
     return wb_personal
+
 
 def tear_down_personal(wb_personal):
     wb_personal.close()
+
 
 if __name__ == '__main__':
     excel_file_path = r'C:\Users\CESAR\Documents\GitHub\bot_dropbox_python\teste_macro.xlsb'
